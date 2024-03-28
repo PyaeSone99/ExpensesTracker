6c224d7eda5d9892125b425323be7d2bf30b70ca
pyaesoneaung <pyaesone@blue-stone.net>
2024-03-28 22:42:19 +0630

    128 - Adding Alternative Constructor Functions Filtering Lists

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -37,6 +37,10 @@ class ExpenseBucket {
     required this.category,
     required this.expenses
   });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses,this.category) : expenses = allExpenses.where(
+    (expense) => expense.category == category
+  ).toList();
   
   final Category category;
   final List<Expense> expenses;
