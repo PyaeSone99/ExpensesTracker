b7f3df29bdcb44830600776a716049b38b4ac58e
Pyae Sone Aung <pyaesone@blue-stone.net>
2024-03-28 16:19:41 +0630

    120 - Using the Dismissible Widget for Dismissing List Items

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -4,15 +4,20 @@ import 'package:expenses_tracker/models/expense.dart';
 
 class ExpensesList extends StatelessWidget {
 
-  const ExpensesList({super.key,required this.expenses});
+  const ExpensesList({super.key,required this.expenses,required this.onRemoveExpense});
 
   final List<Expense> expenses;
+
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (context, index) => ExpensesItem(expenses[index]),
+      itemBuilder: (context, index) => Dismissible(
+          key: ValueKey(expenses[index]), 
+          onDismissed: (direction) => onRemoveExpense(expenses[index]),
+          child: ExpensesItem(expenses[index])),
     );
   } 
 }
